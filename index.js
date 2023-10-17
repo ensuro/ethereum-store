910db9a459c85040dff802074c5a7aa30954956f
ndelatorre <ndelatorre@itba.edu.ar>
2023-10-17 10:00:24 +0200

    initialize store

---
+++ index.js
@@ -0,0 +1,51 @@
+export { ethereumSaga } from "./saga";
+export { EthereumReducer } from "./reducer";
+
+// Helpers
+let getEncodedCallFn;
+let getContractFn;
+let getAbiNameFn;
+let getFormatterFn;
+let getSignerContractFn;
+let getTxReceiptStatusFn;
+
+// User
+let selectUserAddressFn;
+let selectChainIdFn;
+let selectProviderFn;
+
+export function initializeEthereumStore(options) {
+  const {
+    getEncodedCall,
+    getContract,
+    getAbiName,
+    getFormatter,
+    getSignerContract,
+    getTxReceiptStatus,
+    selectUserAddress,
+    selectChainId,
+    selectProvider,
+  } = options;
+
+  getEncodedCallFn = getEncodedCall;
+  getContractFn = getContract;
+  getAbiNameFn = getAbiName;
+  getFormatterFn = getFormatter;
+  getSignerContractFn = getSignerContract;
+  getTxReceiptStatusFn = getTxReceiptStatus;
+  selectUserAddressFn = selectUserAddress;
+  selectChainIdFn = selectChainId;
+  selectProviderFn = selectProvider;
+}
+
+export {
+  getEncodedCallFn,
+  getContractFn,
+  getAbiNameFn,
+  getFormatterFn,
+  getSignerContractFn,
+  getTxReceiptStatusFn,
+  selectUserAddressFn,
+  selectChainIdFn,
+  selectProviderFn,
+};
